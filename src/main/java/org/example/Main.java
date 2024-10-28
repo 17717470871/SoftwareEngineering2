9460cdfa4a1431bf0fb3f3ee5b2ad745ec7a7d6e
RU <2966031606@qq.com>
2024-10-28 19:27:00 +0800

    LiSi created a new branch.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hi,I am ZhangSan.I created a new program");
-            System.out.println("Hi,I am LiSi.I modified this program");
+            System.out.println("Hi,I am ZhangSan.I created a new program.");
+            System.out.println("Hi,I am LiSi.I modified this program.");
+            System.out.println("Hi,I am LiSi.I created a new branch.");
         }
     }
